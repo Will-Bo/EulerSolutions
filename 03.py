e669a62507f7078d8c03e1493cc7c15a52644f10
Will-Bo <awill4me@gmail.com>
2022-07-24 09:05:39 -0400

    Added Solution #2 to Problem 3

--- 03.py
+++ 03.py
@@ -35,14 +35,57 @@ def seiveOfEratosthenes(n):
 
     return arr
 
-
-## Declaring n as 600851475143, then returning the highest prime factor
-def main():
-    n = 600851475143
-
+## Runner function for Solution #1
+def solution1Runner(n):
     ## Generating every prime number below sqrt(n), then moving through them backwards to find the highest one
     for x in seiveParser(seiveOfEratosthenes(int(n**.5)))[::-1]:
         if n%x == 0:
             return x
-    
-print(main())
+
+
+## Solution #2 -- Actually generate prime factorization and find the largest element
+def primeFactor(n):
+
+    factors = []
+
+    ## Taking out every factor of 2
+    while n%2 == 0:
+        factors.append(2)
+        n = n/2
+
+    ## Moving through every odd number and factoring it out. Anything left by this process (moving up through 3, 5, etc.) will be prime
+    for i in range(3, int(n**.5) + 1, 2):
+        while n%i == 0:
+            factors.append(i)
+            n = n/i
+
+    ## Maybe the resulting n is still a prime factor
+    if n > 2:
+        factors.append(n)
+
+    return factors
+
+## Returning the greatest prime factor
+def findGPF(factors):
+    return max(factors)
+
+
+## Calling the runners for solutions #1 and #2
+def main():
+    n = 600851475143
+    print("Solution #1:",solution1Runner(n))
+    print("Solution #2:",findGPF(primeFactor(n)))
+
+main()
+
+
+## This function tests how long each solution takes -- showing that Solution #2 is roughly 20 times faster than Solution #1 on my computer (.5 ms versus .026 ms)
+def timeTest():
+    n = 600851475143
+    from time import time
+    start = time()
+    print("Solution #1 of",solution1Runner(n),"takes",time()-start,"milliseconds")
+    start = time()
+    print("Solution #2 of",findGPF(primeFactor(n)),"takes",time()-start,"milliseconds")
+
+#timeTest()
