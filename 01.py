945d100063db8e2eff76ee306f1cf07247faf66f
will <will@localhost.localdomain>
2023-06-11 09:05:41 -0400

    Restructure repo

--- 01.py
+++
@@ -1,9 +0,0 @@
-## If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-## Find the sum of all the multiples of 3 or 5 below 1000.
-
-## Simple solution -- python's built-in functions are the most efficient way to do this
-def sumMultiples(number):
-    return sum([x for x in range(1,number) if (x%3==0 or x%5==0)])
-
-print(sumMultiples(10))
-print(sumMultiples(1000))
--- 16.py
+++
@@ -1,7 +0,0 @@
-## What is the sum of the digits of the number 2**1000?
-
-## where s is the number 2, we just have to loop through digits -- easy with a list comprehension inside of a call to the sum() function
-def main(s):
-    return sum([int(x) for x in str(s**1000)])
-
-print(main(2))
