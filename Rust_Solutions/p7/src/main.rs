6765c0b3bf9fff577d55f98167a85013d316cacf
Will-Bo <awill4me@gmail.com>
2023-06-11 09:43:52 -0400

    Wrote Rust solution for P7

--- Rust_Solutions/p7/src/main.rs
+++ Rust_Solutions/p7/src/main.rs
@@ -1,26 +1,50 @@
 use std::env;
 
+// Using the Sieve of Eratosthenes to find a large number of prime numbers and store them in an
+// array, then use that array to answer the problem
+// https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+//
+// Use: cargo run -- `num`
+// Where `num` is an absurdly large number --- this can be arbitrary
+// At the moment, I have no way of knowing how large of a number I need to generate before I find
+// the necessary prime, so I like to use cargo run -- 100000000
 fn main() {
 
     let args: Vec<String> = env::args().collect();
-    let max:i32 = args[1].parse().unwrap();
-    println!("Max is {}", max);
+    let max:i64 = args[1].parse().unwrap();
+    println!("Finding primes below {}", max);
 
-//    let nums: [i32; max] = [0; max];
-
-    let mut nums: Vec<i32> = vec![Default::default(); max as usize];
+    // initializing the vector of prime indicators
+    let mut nums: Vec<i64> = vec![Default::default(); max as usize];
     for element in nums.iter_mut() {
         *element = 0;
     }
 
-    let mut i:i32 = 2;
-    let mut n:i32;
-    while (i < max){
+    // declaring an empty vector to hold the primes that we find
+    let mut primes: Vec<i64> = vec![Default::default()];
+
+    let mut i:i64 = 2;
+    let mut n:i64;
+    while i < max {
         n = 1;
-        while (n*i < max){
-            println!("{} ", n*i);
+        while n*i < max {
+            nums[(n*i) as usize] = 1;
             n+=1;
         }
-        i+=1;
+        // Adding this prime to the list
+        primes.push(i);
+        // Finding the next prime
+        while i < max && nums[i as usize] != 0 {
+            i += 1;
+        }
     }
+
+
+    // `primes` is our list of the prime numbers
+    // primes[1] is the first prime number, which is 2
+    // The list is prefixed with 0: primes[0] = 0
+
+    // This is the answer to the problem
+    let prime:i64 = 10001;
+    println!("The {}st/nd/th prime is {}", prime, primes[prime as usize]);
 }
---
+++ Rust_Solutions/p7/target/debug/deps/p7-5ceb2a422eebc436.d
@@ -0,0 +1,5 @@
+/home/will/Programming/EulerSolutions/Rust_Solutions/p7/target/debug/deps/p7-5ceb2a422eebc436.rmeta: src/main.rs
+
+/home/will/Programming/EulerSolutions/Rust_Solutions/p7/target/debug/deps/p7-5ceb2a422eebc436.d: src/main.rs
+
+src/main.rs:
